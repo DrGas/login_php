b841cbe60818d4a2ebd9d17042f02cb70ae5367a
Magdalena Drgas <magdalena.a.drgas@gmail.com>
2016-09-28 12:27:38 +0200

    further styling

--- header.php
+++ header.php
@@ -16,10 +16,11 @@
     		<li><a href="index.php">HOME</li>
             <?php 
 				//LOGIN FORM from front page, needs to change "" to '' because it is PHP now
-	//it also needs to be echoed - to be visible	
+	//it also needs to be echoed - to be visible
+	//delete all the breaks between uid and pwd - it makes it easier to style it 	
 	echo "<form action='login.php' method='POST'>
-	 <input type='text' name='uid' placeholder='Username'><br>
-    <input type='password' name='pwd' placeholder='Password'><br>
+	 <input type='text' name='uid' placeholder='Username'>
+    <input type='password' name='pwd' placeholder='Password'>
     <button type='submit'>LOGIN</button>
 	</form>";
 			?>
