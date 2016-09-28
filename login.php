3d1b3d6f19ecf81b2238d2cf93c2e96c9a5b1293
Magdalena Drgas <magdalena.a.drgas@gmail.com>
2016-09-28 10:38:51 +0200

    creating session

--- login.php
+++ login.php
@@ -6,13 +6,14 @@ $uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
 
 //SELECT statement, because it needs to SELECT from already existing data from database
-$sql = "SELECT * user WHERE uid='$uid' AND pwd='$pwd'";
+$sql = "SELECT * FROM user WHERE uid='$uid' AND pwd='$pwd'";
 $result =mysqli_query($conn, $sql);
 
-if (!$row = mysqli_fetch_assoc($result)) {
+if (!$row = mysqli_fetch_assoc($result)){
 	echo "Your username or password is incorrect";
 	} else {
-		echo "You are logged in!";
+		//CERATING SESSION
+		$_SESSION['id'] = $row['id'];
 		}
 
 ?>
