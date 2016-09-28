0eb79428dbe5ebb7619e07f7ca619894aaec643b
Magdalena Drgas <magdalena.a.drgas@gmail.com>
2016-09-28 11:09:01 +0200

    including header
    different file which contains all the data from starting the session to
    <body> and can be pased in all the pages, which makes it easy to make
    later changes - just in one file

---
+++ header.php
@@ -0,0 +1,12 @@
+<?php 
+	session_start();
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Login</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head> 
+<body>
--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<?php 
-	session_start();
-?>
-
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Login</title>
-</head>
 <!--LOGIN FORM-->
 <form action="login.php" method="POST">
 	 <input type="text" name="uid" placeholder="Username"><br>
@@ -26,7 +16,7 @@
 
 <br><br><br>
 <!--SIGN UP FORM-->
-<body>
+
 <form action="signup.php" method="POST">
     <input type="text" name="first" placeholder="Firstname"><br>
     <input type="text" name="last" placeholder="Lastname"><br>
