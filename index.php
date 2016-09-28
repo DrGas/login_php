e72f729120aed3e69be6b7f1966071d2f441ddf3
Magdalena Drgas <magdalena.a.drgas@gmail.com>
2016-09-28 10:13:36 +0200

    creating login form
    creating login form similar to sign up form, but just with user id and
    password

--- index.php
+++ index.php
@@ -4,7 +4,15 @@
 <meta charset="utf-8">
 <title>Login</title>
 </head>
+<!--LOGIN FORM-->
+<form action="login.php" method="POST">
+	 <input type="text" name="uid" placeholder="Username"><br>
+    <input type="password" name="pwd" placeholder="Password"><br>
+    <button type="submit">LOGIN</button>
+</form>
 
+<br><br><br>
+<!--SIGN UP FORM-->
 <body>
 <form action="signup.php" method="POST">
     <input type="text" name="first" placeholder="Firstname"><br>
