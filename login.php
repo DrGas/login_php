143e07d2b7940926946ff5558f6daca2a487337c
Magdalena Drgas <magdalena.a.drgas@gmail.com>
2016-09-28 10:22:31 +0200

    SELECT FROM
    creating SELECT from, because we already selecting exisitng data from DB
    and also checking if there is any result in the database, if not then
    echo "You username/password is incorrect";

--- login.php
+++ login.php
@@ -1,10 +1,19 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php 
 
-<body>
-</body>
-</html>
+include 'dbh.php';
+
+$uid = $_POST['uid'];
+$pwd = $_POST['pwd'];
+
+//SELECT statement, because it needs to SELECT from already existing data from database
+$sql = "SELECT * user WHERE uid='$uid' AND pwd='$pwd'";
+$result =mysqli_query($conn, $sql);
+
+if (!$row = mysqli_fetch_assoc($result)) {
+	echo "Your username or password is incorrect";
+	} else {
+		echo "You are logged in!";
+		}
+
+header("Location: index.php");
+?>
--- signup.php
+++ signup.php
@@ -7,6 +7,7 @@ $last = $_POST['last'];
 $uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
 
+//INSERT INTO becasue we are puting new information into the DB
 $sql = "INSERT INTO user (first, last, uid, pwd) 
 VALUES ('$first', '$last', '$uid', '$pwd')";
 $result =mysqli_query($conn, $sql);
