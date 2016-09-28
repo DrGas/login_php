fe17c310fa5bce205e85b49ea4db2e0428740747
Magdalena Drgas <magdalena.a.drgas@gmail.com>
2016-09-28 10:51:42 +0200

    creating log out button

--- index.php
+++ index.php
@@ -15,10 +15,13 @@
     <button type="submit">LOGIN</button>
 </form>
 
+<!--SESSION echoing session ID to the frontpage-->
 <?php 
 	if (isset($_SESSION['id'])) {
 		echo $_SESSION['id'];
-		}
+		} else {
+			echo "You are not logged in";
+			}
 ?>
 
 <br><br><br>
@@ -31,4 +34,11 @@
     <input type="password" name="pwd" placeholder="Password"><br>
     <button type="submit">SIGN UP</button>
 </form>
+
+<br><br><br>
+<!--LOG OUT, we don't need to put "method" because we just need to run this one file -->
+<form action="logout.php">
+	<button>LOG OUT</button>
+</form>
+
 </html>
