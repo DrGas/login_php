c463a476cc56ad4d5095fea08a9fabf3bbe15e32
Magdalena Drgas <magdalena.a.drgas@gmail.com>
2016-09-28 10:03:09 +0200

    connnection to database 2
    testing my login with echo and delitling it afterwards, continue on the
    connection to the database with INSERT into the table

--- signup.php
+++ signup.php
@@ -7,8 +7,7 @@ $last = $_POST['last'];
 $uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
 
-echo $first."<br>";
-echo $last."<br>";
-echo $uid."<br>";
-echo $pwd."<br>";
+$sql = "INSERT INTO user (first, last, uid, pwd) 
+VALUES ('$first', '$last', '$uid', '$pwd',)";
+$result =mysqli_query($conn, $sql);
 ?>
