a9a343bf19f77e05a419d4f38dfa0e2f2e6b96d1
Magdalena Drgas <magdalena.a.drgas@gmail.com>
2016-09-29 10:48:55 +0200

    css and connecting all the login and logout pages

--- index.php
+++ index.php
@@ -10,10 +10,5 @@ include 'header.php';
 			echo "You are not logged in";
 			}
 ?>
-<br><br><br>
-<!--LOG OUT, we don't need to put "method" because we just need to run this one file -->
-<form action="logout.php">
-	<button>LOG OUT</button>
-</form>
 
 </html>
--- signup-page.php
+++ signup-page.php
@@ -4,21 +4,21 @@ include 'header.php';
 <!--SESSION echoing session ID to the frontpage-->
 <?php 
 	if (isset($_SESSION['id'])) {
-		echo $_SESSION['id'];
+		echo "You are already logged in!";
 		} else {
-			echo "You are not logged in";
+			echo "<form action='signup.php' method='POST'>
+    <input type='text' name='first' placeholder='Firstname'><br>
+    <input type='text' name='last' placeholder='Lastname'><br>
+    <input type='text' name='uid' placeholder='Username'><br>
+    <input type='password' name='pwd' placeholder='Password'><br>
+    <button type='submit'>SIGN UP</button>
+</form>";
 			}
 ?>
 
 <br><br><br>
 <!--SIGN UP FORM-->
 
-<form action="signup.php" method="POST">
-    <input type="text" name="first" placeholder="Firstname"><br>
-    <input type="text" name="last" placeholder="Lastname"><br>
-    <input type="text" name="uid" placeholder="Username"><br>
-    <input type="password" name="pwd" placeholder="Password"><br>
-    <button type="submit">SIGN UP</button>
-</form>
+
 
 </html>
